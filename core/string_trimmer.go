50cd1733ab6f8397f4460f21c1d5e62a225dd6a0
Jamie <j.osler@gmail.com>
2019-06-24 17:03:59 -0400

    default trim length instead of panic

--- core/string_trimmer.go
+++ core/string_trimmer.go
@@ -6,8 +6,13 @@ import (
 	"golang.org/x/crypto/ssh/terminal"
 )
 
+const DefaultTrimAtLength = 120
+
 func TrimString(input string, prefixLength int) string {
-	trimAt, _, _ := terminal.GetSize(int(os.Stdout.Fd()))
+	trimAt, _, err := terminal.GetSize(int(os.Stdout.Fd()))
+	if err != nil {
+		trimAt = DefaultTrimAtLength
+	}
 	trimAt = trimAt - prefixLength
 	if len(input) < trimAt {
 		trimAt = len(input)
