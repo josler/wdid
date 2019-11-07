5a6e74447d15d7b9128c4d5998f8126a3acef595
josler <j.osler@gmail.com>
2019-11-06 19:24:36 -0500

    add ellipsis

---
+++ core/string_trimmer_test.go
@@ -0,0 +1,17 @@
+package core
+
+import (
+	"testing"
+
+	"gotest.tools/assert"
+)
+
+func TestStringTrimmerTrimsExtraCharacters(t *testing.T) {
+	trimmed := TrimString("my input", DefaultTrimAtLength-len("my"))
+	assert.Equal(t, trimmed, "my…")
+}
+
+func TestStringTrimmerPreservesNoEllipsisIfInputSmaller(t *testing.T) {
+	trimmed := TrimString("small", DefaultTrimAtLength-20)
+	assert.Equal(t, trimmed, "small")
+}
